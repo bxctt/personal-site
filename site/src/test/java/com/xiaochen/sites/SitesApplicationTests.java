e76443e4657483db2a1da090d711a47ba04fc33e
unknown <xiaochen.bian@borngroup.com>
2025-08-19 00:39:20 -0400

    init commit

---
+++ site/src/test/java/com/xiaochen/sites/SitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaochen.sites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
